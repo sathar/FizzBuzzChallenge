c5ac879ac8b6011dc5fc72e8c226d153b8a7c5b2
Satha Raveendran <satha_r@hotmail.com>
2017-06-30 00:00:50 +0100

    Test Methods included

---
+++ FizzBuzzTestProject/FizzBuzzTest.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using FizzBuzzChallenge;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzzTestProject
+{
+    [TestClass]
+    public class FizzBuzzTest
+    {
+        [TestMethod]
+        public void MultiplesOfThreePrintFizz()
+        {
+            //Arrange
+            int input = 6;
+
+            //Act
+            using (ConsoleRedirector cr = new ConsoleRedirector())
+            {
+                IFizzBuzzCommandProcessor fizzBuzzPrintCommandProccessor = new FizzBuzzCommandProcessor();
+                fizzBuzzPrintCommandProccessor.Process(input);
+                //Assert
+
+                Assert.IsTrue(cr.ToString().Contains("Fizz"));
+            }
+        }
+
+        [TestMethod]
+        public void MultiplesOfFivePrintBuzz()
+        {
+            //Arrange
+            int input = 25;
+
+            //Act
+            using (ConsoleRedirector cr = new ConsoleRedirector())
+            {
+                IFizzBuzzCommandProcessor fizzBuzzPrintCommandProccessor = new FizzBuzzCommandProcessor();
+                fizzBuzzPrintCommandProccessor.Process(input);
+                //Assert
+
+                Assert.IsTrue(cr.ToString().Contains("Buzz"));
+            }
+        }
+
+        [TestMethod]
+        public void MultiplesOfThreeAndFivePrintFizz()
+        {
+            //Arrange
+            int input = 15;
+
+            //Act
+            using (ConsoleRedirector cr = new ConsoleRedirector())
+            {
+                IFizzBuzzCommandProcessor fizzBuzzPrintCommandProccessor = new FizzBuzzCommandProcessor();
+                fizzBuzzPrintCommandProccessor.Process(input);
+                //Assert
+
+                Assert.IsTrue(cr.ToString().Contains("Fizz"));
+            }
+        }
+
+        [TestMethod]
+        public void MultiplesOfThreeAndFivePrintBuzz()
+        {
+            //Arrange
+            int input = 15;
+
+            //Act
+            using (ConsoleRedirector cr = new ConsoleRedirector())
+            {
+                IFizzBuzzCommandProcessor fizzBuzzPrintCommandProccessor = new FizzBuzzCommandProcessor();
+                fizzBuzzPrintCommandProccessor.Process(input);
+                //Assert
+
+                Assert.IsTrue(cr.ToString().Contains("Buzz"));
+            }
+        }
+
+        [TestMethod]
+        public void NitherMultiplesOfThreeNorFivePrintNothing()
+        {
+            //Arrange
+            int input = 14;
+
+            //Act
+            using (ConsoleRedirector cr = new ConsoleRedirector())
+            {
+                IFizzBuzzCommandProcessor fizzBuzzPrintCommandProccessor = new FizzBuzzCommandProcessor();
+                fizzBuzzPrintCommandProccessor.Process(input);
+                //Assert
+
+                Assert.IsTrue(cr.ToString().Contains(""));
+            }
+        }
+
+    }
+}
