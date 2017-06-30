53e0cc231e77d40972dbf0f0344cd054016e078c
Satha Raveendran <satha_r@hotmail.com>
2017-06-30 06:44:20 +0100

    non numerical input do nothing

--- FizzBuzzChallenge/Program.cs
+++ FizzBuzzChallenge/Program.cs
@@ -15,12 +15,8 @@ namespace FizzBuzzChallenge
             {
                 var input = Console.ReadLine();
                 int result;
-                if (!int.TryParse(input, out result))
-                {
-                    Console.WriteLine("Digit required");
-                }
-                else
-                {
+                if (int.TryParse(input, out result))
+                {              
                     IFizzBuzzCommandProcessor fizzBuzzPrintCommandProccessor = new FizzBuzzCommandProcessor();
                     fizzBuzzPrintCommandProccessor.Process(Int32.Parse(input));
                 }
